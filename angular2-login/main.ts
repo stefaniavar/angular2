9069af72a76f7c0898ef6b8422a5c9ae026ee123
stefaniavar <dittastefania@yahoo.it>
2017-05-15 17:55:38 +0200

    Update main.ts

--- angular2-login/main.ts
+++ angular2-login/main.ts
@@ -1,1 +1,6 @@
 
+import {bootstrap} from 'angular2/platform/browser';
+import {AppComponent} from './app.component'
+import{ROUTER_PROVIDER} from 'angular2/router'
+
+bootstrap(AppComponent, [ROUTER_PROVIDER]);
