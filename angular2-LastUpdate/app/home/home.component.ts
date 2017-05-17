89c043c8dc923fc962ee3b01dc07c320f45f7bd5
stefaniavar <dittastefania@yahoo.it>
2017-05-17 18:03:18 +0200

    Update home.component.ts

--- angular2-LastUpdate/app/home/home.component.ts
+++ angular2-LastUpdate/app/home/home.component.ts
@@ -1,12 +1,17 @@
 import { Component } from 'angular2/core';
 import { AuthenticationService } from '../authentication/authentication.service';
 import { User } from '../authentication/user';
+import { ROUTER_DIRECTIVES} from 'angular2/router';
+import { LoginComponent} from '../login/login.component';
+import { UpdateComponent} from './update.component';
 
 @Component({
     selector: 'login-form',
     providers: [AuthenticationService],
+    directives: [LoginComponent, ROUTER_DIRECTIVES],
     templateUrl: "app/home/home.component.html"
 })
+
 
 
 export class HomeComponent {
