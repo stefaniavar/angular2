4744085767f57beaee186e864cfe547cd3b52838
stefaniavar <dittastefania@yahoo.it>
2017-05-17 18:02:02 +0200

    Update authentication.service.ts

--- angular2-LastUpdate/app/authentication/authentication.service.ts
+++ angular2-LastUpdate/app/authentication/authentication.service.ts
@@ -4,8 +4,9 @@ import { User } from './user';
 
 var users =
   [
-    { email: 'admin@gmail.com', password: 'admin', nome: 'stefania', cognome: 'ditta', dateofBirth: '13/03/1984', country: 'Varese', role: 'admin' },
-    { email: 'user@gmail.com', password: 'user', nome: 'alberto', cognome: 'ditta', dateofBirth: '13/03/1980', country: 'Varese', role: 'user' }
+    {email: 'admin@gmail.com', password: 'admin', nome: 'stefania', cognome: 'ditta', dateofBirth: '13/03/1984', country: 'Varese', role: 'admin' },
+    {email: 'user@gmail.com', password: 'user', nome: 'alberto', cognome: 'ditta', dateofBirth: '13/03/1980', country: 'Varese', role: 'user' },
+    {email: 'roby@gmail.com', password: 'roby', nome: 'roberto', cognome: 'rossi', dateofBirth: '13/03/1986', country: 'Milano', role: 'user'}
   ];
 
 @Injectable()
@@ -23,15 +24,27 @@ export class AuthenticationService {
     this._router.navigate(['Login']);
   }
 
-  login(user) {
+  /*login(user) {
     var authenticatedUser = users.find(u => u.email === user.email);
     if (authenticatedUser) {
       localStorage.setItem("user", JSON.stringify(authenticatedUser));
       this._router.navigate(['Home']);
+     
       return true;
     }
     return false;
 
+  }*/
+
+  login(user){
+    var authenticatedUser=users.find(u => u.email === user.email);
+    if(user.email=="admin"){
+      localStorage.setItem("user", JSON.stringify(authenticatedUser));
+      this._router.navigate(['Home2'])
+    }
+    else{
+       this._router.navigate(['Home'])
+    }
   }
 
   checkCredentials() {
